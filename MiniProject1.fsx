207a0da435a2532dba2bc378dbd6b6aafdb5f8ce
Ainis <ainis@seesense.cc>
2021-03-06 15:45:14 +0200

    Renamed types, added comments and added constants for base price of the cup:

--- MiniProject1.fsx
+++ MiniProject1.fsx
@@ -1,33 +1,41 @@
 open System
 
-type Sizes = 
+//Constants for base price of the cup
+let cupS = 0.5
+let cupM = 1.0
+let cupL = 1.5
+
+///Drink sizes type allows to avoid manually entering a string to a Drink type and creates a comprehensive type
+type Size = 
     | Small = 0
     | Medium = 1
     | Large  = 2
 
-type Drinks =
-   | Coffe of drinktype: String * size: Sizes * price: float
-   | Tea of drinktype: String * size: Sizes * price: float
-   | Juice of drinktype: String * size: Sizes * price: float
+///Drink with three types that define drink type, size of a cup and the price for the drink (cup price excluded)
+type Drink =
+   | Coffee of drinkType: String * size: Size * price: float
+   | Tea of drinkType: String * size: Size * price: float
+   | Juice of drinkType: String * size: Size * price: float
 
 
-let c1 = Coffe(drinktype = "Latte", size = Sizes.Medium, price = 3.14)
-let c2 = Coffe(drinktype = "Cortado", size = Sizes.Small, price = 1.9)
-let c3 = Coffe(drinktype = "Lungo", size = Sizes.Large, price = 2.5)
+let c1 = Coffee(drinkType = "Latte", size = Size.Medium, price = 3.14)
+let c2 = Coffee(drinkType = "Cortado", size = Size.Small, price = 1.9)
+let c3 = Coffee(drinkType = "Lungo", size = Size.Large, price = 2.5)
 
-let t1 = Tea(drinktype = "Chai", size = Sizes.Medium, price = 5.0)
-let t2 = Tea(drinktype = "Green tea", size = Sizes.Small, price = 6.0)
-let t3 = Tea(drinktype = "Black tea", size = Sizes.Small, price = 5.5)
+let t1 = Tea(drinkType = "Chai", size = Size.Medium, price = 5.0)
+let t2 = Tea(drinkType = "Green tea", size = Size.Small, price = 6.0)
+let t3 = Tea(drinkType = "Black tea", size = Size.Small, price = 5.5)
 
-let j1 = Juice(drinktype = "Orange juice", size = Sizes.Large, price = 4.9)
-let j2 = Juice(drinktype = "Apple juice", size = Sizes.Large, price = 5.1)
-let j3 = Juice(drinktype = "Pineaple juice", size = Sizes.Medium, price = 3.2)
+let j1 = Juice(drinkType = "Orange juice", size = Size.Large, price = 4.9)
+let j2 = Juice(drinkType = "Apple juice", size = Size.Large, price = 5.1)
+let j3 = Juice(drinkType = "Pineaple juice", size = Size.Medium, price = 3.2)
 
 
-let price (s:Drinks):float =
+///Calculates drink price by adding drink price with the cup price
+let calculateDrinkPrice s =
    match s with
-   | Coffe(t,s,p) -> if s=Sizes.Small then p + 30.0 elif s=Sizes.Medium then p + 20.0 else p + 40.0
-   | Tea(t,s,p) -> if s=Sizes.Small then p + 30.0 elif s=Sizes.Medium then p + 20.0 else p + 40.0
-   | Juice(t,s,p) -> if s=Sizes.Small then p + 30.0 elif s=Sizes.Medium then p + 20.0 else p + 40.0
+   | Coffee(t,s,p) -> if s=Size.Small then p + cupS elif s=Size.Medium then p + cupM else p + cupL
+   | Tea(t,s,p) -> if s=Size.Small then p + cupS elif s=Size.Medium then p + cupM else p + cupL
+   | Juice(t,s,p) -> if s=Size.Small then p + cupS elif s=Size.Medium then p + cupM else p + cupL
 
-price j1
+let testPrice = calculateDrinkPrice j1
